b2eae4e6729800a7c268b7b2f006757907a4326c
Natalya Ivanova <natalyas.ivanova@gmail.com>
2017-01-07 22:18:42 +0200

    Perform search using gcse API 2.0

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -1,31 +1,25 @@
 var searchKey = "017149004764189221444:_3kraw1vh5q";
-var apiKey = "AIzaSyAz7QmmWw4ECBLs4TCo7YXWdUVw3bRrme0";
 
 document.forms[0].addEventListener("submit", function(e) {
     e.preventDefault();
-    search.setAttribute("data-queryParameterName", query.value);
-    console.log(query.value);
-
-    var xhr = new XMLHttpRequest();
-    // xhr.open("GET", "https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&amp;cx=" + searchKey + "&amp;q=" + query.value + "&amp;callback=hndlr", true);
-    // xhr.open("GET", "https://www.googleapis.com/customsearch/v1?key=AIzaSyAz7QmmWw4ECBLs4TCo7YXWdUVw3bRrme0&amp;cx=017149004764189221444:_3kraw1vh5q&amp;q=Мушля", true);
-    xhr.open("GET", "https://www.googleapis.com/customsearch/v1?key=AIzaSyAz7QmmWw4ECBLs4TCo7YXWdUVw3bRrme0&amp;cx=017149004764189221444:_3kraw1vh5q&amp;q=Мушля&amp;callback=hndlr", true);
-    // xhr.onreadystatechange = function() {
-    //     console.log(xhr.responseText);
-    //     for (var i = 0; i < response.items.length; i++) {
-    //         var item = response.items[i];
-    //         // in production code, item.htmlTitle should have the HTML entities escaped.
-    //         document.getElementById("content").innerHTML += "<br>" + item.htmlTitle;
-    //     }
-    // }
-    xhr.send(null);
+    google.search.cse.element.getElement("results").execute(query.value);
 });
 
-  function hndlr(response) {
-      console.log(response);
-      for (var i = 0; i < response.items.length; i++) {
-        var item = response.items[i];
-        // in production code, item.htmlTitle should have the HTML entities escaped.
-        document.getElementById("content").innerHTML += "<br>" + item.htmlTitle;
-      }
-  }
+window.__gcse = {
+    parsetags: 'explicit',
+    callback: myCallback
+};
+
+var gcse = document.createElement('script');
+gcse.type = 'text/javascript';
+gcse.async = true;
+gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + searchKey;
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+
+function myCallback() {
+    google.search.cse.element.render({
+        div: "search",
+        tag: "searchresults-only",
+        gname: "results"
+    });
+}
