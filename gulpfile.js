96d507f0e3f28331e4f00b41c0a97de5b8011abb
Natalya Ivanova <natalyas.ivanova@gmail.com>
2017-01-07 23:37:50 +0200

    Install and configure gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    cleanCSS = require('gulp-clean-css'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+gulp.task('default', function() {
+    console.log("hello");
+});
