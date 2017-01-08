2b03d4d8aaeadea42668ecf64a0d65b9da2d8147
Natalya Ivanova <natalyas.ivanova@gmail.com>
2017-01-08 02:10:39 +0200

    Add some explanatory comments to ajax.js

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -8,23 +8,27 @@ document.forms[0].addEventListener("submit", function(e) {
     google.search.cse.element.getElement("results").execute(q);
 });
 
+// initialize google custorm search object
 window.__gcse = {
     parsetags: 'explicit',
     callback: myCallback
 };
 
+// initialize gcs element
 var gcse = document.createElement('script');
 gcse.type = 'text/javascript';
 gcse.async = true;
 gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + searchKey;
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
 
+// call after gcse is initiated
 function myCallback() {
     google.search.cse.element.render({
         div: "search",
         tag: "searchresults-only",
         gname: "results"
     });
+    // for initial page load, get results from ?query=
     if (qstring.length > 0) {
         q = decodeURIComponent(qstring.substr(7));
         query.value = q;
